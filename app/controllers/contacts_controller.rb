c9ef4e40e19ff87f39fa6982063d54df62d4faa4
Stephanie Nemeth <traumverloren@gmail.com>
2014-12-27 14:54:25 +0100

    added flash danger msg to contact form

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -8,9 +8,10 @@ class ContactsController < ApplicationController
     @contact = Contact.new(secure_params)
     if @contact.valid?
       UserMailer.contact_email(@contact).deliver
-      flash[:success] = "Message sent from #{@contact.name}."
+      flash.now[:success] = "Message sent from #{@contact.name}."
       redirect_to new_contact_path
     else
+      flash.now[:danger] = "Message not sent.  Please see errors below."
       render :new
     end
   end
